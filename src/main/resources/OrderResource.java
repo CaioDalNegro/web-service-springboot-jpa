b6432eb49a1c6be0d263c9562ddf09855b5094df
CaioDalNegro <caiodalnegro@gmail.com>
2025-08-16 02:35:09 -0300

    Order, Instant, ISO 8601

---
+++ src/main/resources/OrderResource.java
@@ -0,0 +1,35 @@
+package br.com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.educandoweb.course.entities.Order;
+import br.com.educandoweb.course.service.Order;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderResource {
+    
+    @Autowired
+    private OrderService service;
+
+    // Busca todos os usuários
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll(){
+        List<Order> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    // Busca usuário por ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id) {
+        Order obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
